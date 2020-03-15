0504d1ddf61ee8243e4fa80b4b36376e3d55558b
ZaynTN <51265715+ZaynTN@users.noreply.github.com>
2020-03-15 19:51:21 +0100

    Add files via upload

---
+++ suite.pas
@@ -0,0 +1,21 @@
+﻿program suit;
+uses wincrt;
+var
+    U:real;
+
+procedure suite(var U:real);
+var
+    l:real;
+begin
+	U:=2;
+	l:=0.0001;
+	U:=sqrt(U);
+	while(U>=l) do
+	begin
+		writeln(U:2:2);
+		U:=sqrt(U);
+    end;
+end;
+begin
+	suite(U);
+end.
